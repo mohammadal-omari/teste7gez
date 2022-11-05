b1494ce32fece28f61f620c9ee6eff0824ef1a40
mostafa almomani <momanimostafa98@gmail.com>
2022-11-05 19:43:46 +0300

    fix path

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3002;
 
 app.use(express.static(path.join(__dirname + '/e7gez_super_admin/dist')));
-app.get('/*', (req,res) => res.sendFile(path.join(__dirname + '/e7gez_super_admin/dist/index.html')));
+// app.get('/*', (req,res) => res.sendFile(path.join(__dirname + '/e7gez_super_admin/dist/index.html')));
 
 var server = http.createServer(app);
 server.listen(PORT, () => console.log(`Running ..... on http://localhost:${PORT}`));
