1eeb91a1b9988100d11f28f81262fe0c122a1d98
mostafa almomani <momanimostafa98@gmail.com>
2022-11-05 19:47:23 +0300

    fix path

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3002;
 
-app.use(express.static(path.join(__dirname + '/e7gez_super_admin/dist')));
+app.use(express.static(__dirname + '/e7gez_super_admin/dist'));
 // app.get('/*', (req,res) => res.sendFile(path.join(__dirname + '/e7gez_super_admin/dist/index.html')));
 
 var server = http.createServer(app);
